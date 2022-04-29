c4431789b14f34de1295bf29dbec46f586592b7c
ilhambara <g2a.ilhammubarok@gmail.com>
2022-04-30 01:12:50 +0700

    chore: install and config packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
-import "../styles/globals.css";
+import theme from "../theme/theme";
+import "@fontsource/patua-one";
+import "@fontsource/source-sans-pro";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Layout } from "../components/layout/Layout";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme} resetCSS>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
---
+++ theme/theme.ts
@@ -0,0 +1,24 @@
+// 1. import `extendTheme` function
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+// 2. Add your color mode config
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const theme = extendTheme({
+  fonts: {
+    heading: "Patua One, sans-serif",
+    body: "Source Sans Pro, sans-serif",
+  },
+  // colors: {
+  //   yellow: {
+  //     300: "#FCF24A",
+  //   },
+  // },
+  config,
+});
+
+export default theme;
