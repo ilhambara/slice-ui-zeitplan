dfcabefa8980d799a04e34dd0870419df3779162
ilhambara <g2a.ilhammubarok@gmail.com>
2022-05-11 01:07:31 +0700

    setup: opengraph / meta tags

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,4 +1,10 @@
 const siteConfig = {
+  openGraph: {
+    title: "The Zeitplan – Landing Page UI",
+    desc: "Sebuah aplikasi yang membantu kamu untuk membuat jadwal kuliah, jadwal mabar, jadwal makan, jadwal liga inggris, dan jadwal-jadwal lainnya. Walaupun banyak error, setidaknya kami sudah berusaha semaksimal mungkin.",
+    url: "https://zeitplan.pages.dev/",
+    imgUrl: "https://github.com/ilhambara/slice-ui-zeitplan/blob/main/public/img/og-image.png",
+  },
   navlink: {
     Home: "/",
     Fitur: "/#fitur-fitur",
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,36 @@ import { Features } from "../components/sections/Features";
 import { Pricing } from "../components/sections/Pricing";
 import { Testimonial } from "../components/sections/Testimonial";
 import { Download } from "../components/sections/Download";
+import siteConfig from "../config/siteConfig";
 
 const Home: NextPage = () => {
+  const openGraph = siteConfig.openGraph;
+
   return (
     <>
       <Head>
-        <title>The Zeitplan &ndash; Landing Page UI</title>
-        <meta name="description" content="A custom boilerplate to get the fastest setup for Next.js with Chakra UI" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
+
+        <title>{openGraph.title}</title>
+        <meta name="description" content={openGraph.desc} />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://zeitplan.pages.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={openGraph.title} />
+        <meta property="og:description" content={openGraph.desc} />
+        <meta property="og:image" content={openGraph.imgUrl} />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="zeitplan.pages.dev" />
+        <meta property="twitter:url" content={openGraph.url} />
+        <meta name="twitter:title" content={openGraph.title} />
+        <meta name="twitter:description" content={openGraph.desc} />
+        <meta name="twitter:image" content={openGraph.imgUrl} />
       </Head>
 
       <Hero />
