dd68351ac1c2df4d45bc1da919423dd8dcf76d6b
ilhambara <g2a.ilhammubarok@gmail.com>
2022-05-11 01:11:20 +0700

    fix: og-image not load and edit description

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,9 +1,9 @@
 const siteConfig = {
   openGraph: {
     title: "The Zeitplan – Landing Page UI",
-    desc: "Sebuah aplikasi yang membantu kamu untuk membuat jadwal kuliah, jadwal mabar, jadwal makan, jadwal liga inggris, dan jadwal-jadwal lainnya. Walaupun banyak error, setidaknya kami sudah berusaha semaksimal mungkin.",
+    desc: "Sebuah aplikasi yang membantu kamu untuk membuat jadwal kuliah, jadwal mabar, jadwal makan, jadwal liga inggris, dan jadwal-jadwal lainnya.",
     url: "https://zeitplan.pages.dev/",
-    imgUrl: "https://github.com/ilhambara/slice-ui-zeitplan/blob/main/public/img/og-image.png",
+    imgUrl: "https://raw.githubusercontent.com/ilhambara/slice-ui-zeitplan/main/public/img/og-image.png",
   },
   navlink: {
     Home: "/",
